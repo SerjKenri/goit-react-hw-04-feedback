cf4b0bcc1c157ab53c060860499d991bfbf333fd
Sergey Kuzin <105937658+SerjKenri@users.noreply.github.com>
2023-02-03 00:24:32 +0200

    fix prettier

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,76 +1,77 @@
-import React, { useState } from "react";
-import { Section } from "./Section/Section";
-import { Feedback } from "./Feedback/Feedback";
-import { Statistic } from "./Statistic/Statistic";
-import { Notification } from "./Notification/Notification";
+import React, { useState } from 'react';
+import { Section } from './Section/Section';
+import { Feedback } from './Feedback/Feedback';
+import { Statistic } from './Statistic/Statistic';
+import { Notification } from './Notification/Notification';
 
+export function App() {
+    const [good, setGood] = useState(0);
+    const [neutral, setNeutral] = useState(0);
+    const [bad, setBad] = useState(0);
 
-export function App () {
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
+    const feedBackOptions = { good, neutral, bad };
 
-  const feedBackOptions = {good, neutral, bad};
+    const handleFeedback = e => {
+        const { name } = e.target;
+        switch (name) {
+            case 'good':
+                setGood(prev => prev + 1);
+                break;
+            case 'neutral':
+                setNeutral(prev => prev + 1);
+                break;
+            case 'bad':
+                setBad(prev => prev + 1);
+                break;
+            default:
+                return;
+        }
+    };
 
-  const handleFeedback = e => {
-    const { name } = e.target;
-    switch (name) {
-      case 'good':
-        setGood(prev => prev + 1)
-        break;
-      case 'neutral':
-        setNeutral(prev => prev + 1)
-        break;
-      case 'bad':
-        setBad(prev => prev + 1)
-        break;
-      default:
-        return;
-    }
-  };
+    const totalFeedback = () => {
+        return good + neutral + bad;
+    };
 
-  const totalFeedback = () => {
-    return good + neutral + bad;
-  };
-
-  const positivePercentage = () => {
-
-    if (totalFeedback() === 0) {
-      return 0;
-    }
-    return Math.round((good / totalFeedback()) * 100);
-  };
+    const positivePercentage = () => {
+        if (totalFeedback() === 0) {
+            return 0;
+        }
+        return Math.round((good / totalFeedback()) * 100);
+    };
 
     return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        flexDirection: 'column',
-        textAlign: "center",
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      <Section title="Please leave feedback">
-        <Feedback options={Object.keys(feedBackOptions)} onFeedback={handleFeedback}/>
-      </Section>
+        <div
+            style={{
+                height: '100vh',
+                display: 'flex',
+                flexDirection: 'column',
+                textAlign: 'center',
+                justifyContent: 'center',
+                alignItems: 'center',
+                fontSize: 40,
+                color: '#010101',
+            }}
+        >
+            <Section title="Please leave feedback">
+                <Feedback
+                    options={Object.keys(feedBackOptions)}
+                    onFeedback={handleFeedback}
+                />
+            </Section>
 
-      <Section title="Statistics">
-      {totalFeedback() !== 0 ? (
-            <Statistic
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              total={totalFeedback()}
-              positiveFeed={positivePercentage()}
-            />
-          ) : (
-            <Notification warningText="There is no feedback" />
-          )}
-      </Section>
-    </div>
-  );
-};
+            <Section title="Statistics">
+                {totalFeedback() !== 0 ? (
+                    <Statistic
+                        good={good}
+                        neutral={neutral}
+                        bad={bad}
+                        total={totalFeedback()}
+                        positiveFeed={positivePercentage()}
+                    />
+                ) : (
+                    <Notification warningText="There is no feedback" />
+                )}
+            </Section>
+        </div>
+    );
+}
